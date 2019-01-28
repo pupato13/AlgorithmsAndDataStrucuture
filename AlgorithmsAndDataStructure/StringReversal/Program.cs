199cc4ddb63ae22ff2e50de6fb5560c2ae5c282c
pupato13 <pupato.diego@gmail.com>
2019-01-28 19:54:28 +1300

    Finish the implementation of String Reversal using C#

--- AlgorithmsAndDataStructure/StringReversal/Program.cs
+++ AlgorithmsAndDataStructure/StringReversal/Program.cs
@@ -10,6 +10,50 @@ namespace StringReversal
     {
         static void Main(string[] args)
         {
+            var text = "string reversal";
+            var comparisonTest = "lasrever gnirts";
+
+            var reverseReturn = ReverseWithFor(text);
+            
+            //var reverseReturn = ReverseWithAggregate(text);
+
+            if (IsEqual(comparisonTest, reverseReturn))
+                Console.WriteLine(string.Format("Congrats, the text {0} is {1} in Reversal Mode!", text, reverseReturn));
+            else
+                Console.WriteLine("Text is different, sorry!");
+
+            Console.ReadLine();
+        }
+
+        private static string ReverseWithFor(string text)
+        {
+            var textArr = text.ToArray();
+            var reverseReturn = String.Empty;
+
+            for (int i = 1; i <= textArr.Length; i++)
+            {
+                var letter = textArr[textArr.Length - i];
+
+                reverseReturn += letter;
+            }
+
+            return reverseReturn;
+        }
+
+        private static bool IsEqual(string text, string textComparison)
+        {
+            if (text == textComparison)
+                return true;
+
+            return false;
+        }
+
+        private static string ReverseWithAggregate(string text)
+        {
+            var arr = text.ToArray();
+            var reversed = arr.Aggregate("", (s, c) => c + s);
+
+            return reversed;
         }
     }
 }
