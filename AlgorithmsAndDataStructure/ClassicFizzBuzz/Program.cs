babb45439de29a90002e86c02bed4e11ffceafc7
pupato13 <pupato.diego@gmail.com>
2019-01-29 10:59:46 +1300

    Solving the classic Fizz Buzz problem with style using C#.

---
+++ AlgorithmsAndDataStructure/ClassicFizzBuzz/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassicFizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            //2
+            //Fizz
+            //4
+            //Buzz
+
+            FizzBuzz(-100);
+
+            Console.ReadLine();
+        }
+
+        private static void FizzBuzz(int number)
+        {
+            for (int i = 1; i <= number; i++)
+            {
+                bool fizz = i % 3 == 0;
+                bool buzz = i % 5 == 0;
+
+                if (fizz && buzz)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (fizz)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (buzz)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
